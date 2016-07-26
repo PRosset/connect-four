e282b37d1426797b0e73a6218c1417991cb67342
Patrick <pqrosset@gmail.com>
2016-07-26 18:20:38 -0400

    Trying out transparceny, need to attempt to get a better understanding

---
+++ main.js
@@ -0,0 +1,14 @@
+// newBoard = [[0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0],]
+
+var cols = document.getElementsByClassName('col');
+
+for (i = 0; i < cols; i++) {
+  //Add rows to each column
+  for (j = 0; j < 6; j++) {
+  }
+}
