6c181e0b14f64cd3f318ed3f13a30d1c7a307700
Patrick <pqrosset@gmail.com>
2016-07-27 14:30:22 -0400

    updated gameBoard to build through javascript, as well as storing it in a 2d Array

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-// newBoard = [[0, 0, 0, 0, 0, 0, 0],
-//             [0, 0, 0, 0, 0, 0, 0],
-//             [0, 0, 0, 0, 0, 0, 0],
-//             [0, 0, 0, 0, 0, 0, 0],
-//             [0, 0, 0, 0, 0, 0, 0],
-//             [0, 0, 0, 0, 0, 0, 0],]
+// Need to build a board out of a 2d array.
+// newBoard = [[0, 1, 2, 3, 4, 5],
+//             [0, 1, 2, 3, 4, 5],
+//             [0, 1, 2, 3, 4, 5],
+//             [0, 1, 2, 3, 4, 5],
+//             [0, 1, 2, 3, 4, 5],
+//             [0, 1, 2, 3, 4, 5],
+//             [0, 1, 2, 3, 4, 5]]
+var slotStates = ['empty', 'player1', 'player2'];
 
-var cols = document.getElementsByClassName('col');
+buildBoard();
 
-for (i = 0; i < cols; i++) {
-  //Add rows to each column
-  for (j = 0; j < 6; j++) {
+function buildBoard () {
+  var gameBoard = [];
+  var domBoard = document.getElementById('gameBoard');
+
+  for (i = 0; i < 7; i++) {
+    gameBoard[i] = [];
+    var col = document.createElement('div');
+    col.className = 'col col' + i;
+    col.addEventListener('click', dropToken);
+    domBoard.appendChild(col);
+
+    // Add rows to each column
+    for (j = 0; j < 6; j++) {
+      var slot = document.createElement('div');
+      slot.setAttribute('state', slotStates[0]);
+      slot.className = 'slot slot' + j;
+      col.appendChild(slot);
+      gameBoard[i][j] = slot;
+    }
   }
+  // console.log('gameBoard:', gameBoard);
+}
+
+function dropToken () {
+  this.lastChild.className = "slot player1";
+  console.log('This works! You clicked on: ' + this.className);
 }
