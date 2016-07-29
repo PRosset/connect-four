62a2b31295600daf0867ef061b11c83c9400b727
Patrick <pqrosset@gmail.com>
2016-07-29 10:47:08 -0400

    Messing around with reset button

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var slotStates = ['empty', 'player1', 'player2'];
 var playerStart = 1;
 var playerTurn = 1;
 
-
 buildBoard();
+
 
 function buildBoard () {
   var gameBoard = [];
@@ -21,20 +21,47 @@ function buildBoard () {
     for (j = 6; j > 0; j--) {
       var slot = document.createElement('div');
       slot.setAttribute('state', slotStates[0]);
-      slot.className = 'slot slot' + j;
+      slot.className = 'slot';
+      slot.setAttribute('id', 'col ' + i + ' slot ' + j)
       col.appendChild(slot);
       gameBoard[i][j] = slot;
     }
   }
-  // console.log('gameBoard:', gameBoard);
+  // buildResetBtn();
+  console.log('gameBoard:', gameBoard);
 }
-function buildResetBtn () {
-  var resetSlit = document.createElement('div');
 
-  resetSlit.className = ''
-  resetSlit.addEventListener('click', resetBoard);
+// Building a reset button through Javascript.
+// function buildResetBtn () {
+//   var resetWrapper = document.createElement('div');
+//   var resetSlit = document.createElement('div');
+//   var resetBtn = document.createElement('div');
+//   var resetText = document.createElement('span');
+//   var resetBar = document.getElementById('resetBar');
 
-}
+//   // $('resetBar').append('<div id="resetWrapper"><span id="resetText">Reset</span><div id="resetSlit"><div id="resetBtn"></div></div></div>')
+//   resetWrapper.setAttribute('id', 'resetWrapper');
+//   resetSlit.setAttribute('id', 'resetSlit');
+//   resetBtn.setAttribute('id', 'resetBtn');
+//   resetText.setAttribute('id', 'resetText');
+
+//   resetText.innerHTML = 'Reset';
+
+//   resetWrapper.appendChild(resetText);
+//   resetWrapper.appendChild(resetSlit);
+//   resetSlit.appendChild(resetBtn);
+
+//   resetBar.appendChild(resetWrapper);
+
+//   resetWrapper.addEventListener('onmouseover', function() {
+//     console.log('hello');
+//     resetText.setAttribute('class', 'resetTextActive');
+//   });
+//   resetWrapper.addEventListener('onmouseout', function() {
+//     resetText.setAttribute('class', '');
+//   });
+//   resetSlit.addEventListener('click', resetBoard);
+// }
 
 function dropToken () {
   if (playerTurn === 1) {
@@ -46,6 +73,10 @@ function dropToken () {
   playerTurn = 3 - playerTurn;
 
   console.log('This works! You clicked on: ' + this.className);
+}
+
+function tokenFall () {
+  this.firstChild
 }
 
 function resetBoard () {
